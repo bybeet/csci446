9414aeb8a1886b87052bfb825f87903ebff870f4
Travis Bybee <travis.bybee@gmail.com>
2013-01-15 00:31:42 -0700

    Fixed js errors, mostly dealing with getElements from the document.

--- project01/javascript.js
+++ project01/javascript.js
@@ -1,6 +1,5 @@
 function validFeels() {
-    var x = document.form["form"];
-
+    var x = document.forms["form"];
     var feels = x.elements[0].value;
 
     if(feels.length == 0) {
@@ -10,7 +9,7 @@ function validFeels() {
 
 
 function setEventHandlers() {
-    document.getElementsByTagId("form").addEventListener('submit', validFeels, true);
+    document.getElementById("form").addEventListener('submit', validFeels, true);
 }
 
 window.onload=setEventHandlers;
