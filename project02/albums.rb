14feeca315e1a39dc45c84b4d54ecf87c9149a35
Travis Bybee <travis.bybee@gmail.com>
2013-01-28 20:31:12 -0700

    Added functions for rendering the form and the list. Path of "/shutdown" shutdowns the server. Added a 404 response.

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,9 +1,31 @@
 require 'rack'
 
-class HelloWorld
+class Top100Albums
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+  	request = Rack::Request.new(env)
+  	case request.path
+	  	when "/form" then render_form(request)
+	  	when "/list" then render_list(request)
+	  	when "/shutdown" then exit!
+	  	else render_404
+    end
   end
+  
+  def render_404
+  	[404, {"Content-Type" => "text/plain"}, ["404, Nothing here!"]]
+  end
+  
+  def render_form(request)
+  	response = Rack::Response.new
+  	File.open("form.html", "rb") { |form| response.write(form.read) }
+  	response.finish
+  end
+  
+  def render_list(request)
+  	response = Rack::Response.new(request.path)
+  	response.finish
+  end
+  
 end
 
-Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
+Rack::Handler::WEBrick.run Top100Albums.new, :Port => 8080
