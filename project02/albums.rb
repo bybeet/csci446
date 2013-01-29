b2cc64da066a92eccd58392acf4948c47fbcf080
Travis Bybee <travis.bybee@gmail.com>
2013-01-28 22:02:43 -0700

    Changed handling of albums textfile. Albums are inputed into an array of hashes when the class is created. This should make it easier when sorting the albums based on different assets.

--- project02/albums.rb
+++ project02/albums.rb
@@ -3,6 +3,15 @@ require 'rack'
 class Top100Albums
   def call(env)
   	request = Rack::Request.new(env)
+  	albums = File.open("top_100_albums.txt").read
+  	@info = Array.new
+  	i = 1
+  	albums.each_line do |album|
+  		text = album.split(",")
+  		@info.push(["rank"=> i, "title"=> text[0], "year"=> text[1].strip])
+  		i += 1
+  	end
+  	puts @info[0]
   	case request.path
   		when "/" then render_form(request)
 	  	when "/form" then render_form(request)
@@ -28,19 +37,23 @@ class Top100Albums
   	response = Rack::Response.new
   	File.open("list.html", "rb") { |list| response.write(list.read) }
   	response.write("<h2>Sorted by #{request['order'].capitalize}</h2>")
-  	albums = File.open("top_100_albums.txt").read
-  	i = 1
-  	albums.each_line do |album|
-  		info = album.split(",")
-  		response.write(
-  			"<tr>
-  			<td>#{i}</td>
-  			<td>#{info[0]}</td>
-  			<td>#{info[1]}</td>
+  	@info.each do |album, index|
+  		if album['rank'] == request['rank']
+  			response_write = "<tr class=\"highlight\">
+  			<td>#{album['rank']}</td>
+  			<td>#{album['title']}</td>
+  			<td>#{album['year']}</td>
   			</tr>"
-  		)
-  		i += 1
+  		else
+  			response_write = "<tr>
+  			<td>#{album['rank']}</td>
+  			<td>#{album['title']}</td>
+  			<td>#{album['year']}</td>
+  			</tr>"
+  		end
+  		response.write(response_write)
   	end
+  	puts request['rank']
   	response.finish
   end
   
