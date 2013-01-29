1d658bb4b447db941cf97d9a7348931133e3a8f8
Travis Bybee <travis.bybee@gmail.com>
2013-01-28 22:50:58 -0700

    Finished project02. Added CSS file, modified call method of Top100Albums. Fixed css behaviour issues.

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,21 +1,24 @@
 require 'rack'
 
 class Top100Albums
+	
+	def initialize
+		@info = Array.new
+	  	i = 1
+	  	File.open("top_100_albums.txt").read.each_line do |album|
+	  		text = album.split(",")
+	  		@info.push({ :rank=> i, :name=> text[0], :year=> text[1].strip })
+	  		i += 1
+	  	end
+	end
+
   def call(env)
   	request = Rack::Request.new(env)
-  	#albums = File.open("top_100_albums.txt").read
-  	@info = Array.new
-  	i = 1
-  	File.open("top_100_albums.txt").read.each_line do |album|
-  		text = album.split(",")
-  		@info.push({ :rank=> i, :name=> text[0], :year=> text[1].strip })
-  		i += 1
-  	end
-
   	case request.path
   		when "/" then render_form(request)
 	  	when "/form" then render_form(request)
 	  	when "/list" then render_list(request)
+	  	when "/list.css" then render_list_css
 	  	when "/shutdown" then exit!
 	  	else render_404
     end
@@ -39,7 +42,7 @@ class Top100Albums
   	response.write("<h2>Sorted by #{request['order'].capitalize}</h2>")
   	@info.sort_by { |x| x[request['order'].to_sym] }.each do |album|
   		columns = "<td>#{album[:rank]}</td><td>#{album[:name]}</td><td>#{album[:year]}</td></tr>"
-  		if album[:rank] == request['rank'].to_sym
+  		if album[:rank] == request['rank'].to_i
   			response_write = "<tr class=\"highlight\">" + columns
   		else
   			response_write = "<tr>" + columns
@@ -47,6 +50,12 @@ class Top100Albums
   		response.write(response_write)
   	end
   	response.write(File.open("list2.html", "rb").read)
+  	response.finish
+  end
+  
+  def render_list_css
+  	response = Rack::Response.new
+  	response.write(File.open("list.css", "rb").read)
   	response.finish
   end
   
