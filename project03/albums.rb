14ff35b089190c87980aa922d1a271595063a1ef
Travis Bybee <travis.bybee@gmail.com>
2013-02-03 21:40:03 -0700

    Moved form html and ruby creation of 100 option combobox to erb file.

--- project03/albums.rb
+++ project03/albums.rb
@@ -4,7 +4,14 @@ require 'sqlite3'
 class Top100Albums
 	
 	def initialize
-		@albums = SQLite3::Database.new("albums.sqlite3.db")
+		@info = Array.new
+	  	i = 1
+	  	File.open("top_100_albums.txt").read.each_line do |album|
+	  		text = album.split(",")
+	  		@info.push({ :rank=> i, :name=> text[0], :year=> text[1].strip })
+	  		i += 1
+	  	end
+      #@albums = SQLite3::Database.new("albums.sqlite3.db")
 	end
 
   def call(env)
@@ -25,9 +32,7 @@ class Top100Albums
   
   def render_form(request)
   	response = Rack::Response.new
-  	response.write(File.open("form.html", "rb").read)
-  	1.upto(100) { |i| response.write("<option value=\"#{i}\">#{i}</option>\n") }
-  	response.write(File.open("form2.html", "rb").read)
+  	response.write(ERB.new(File.read("form.erb")).result(binding))
   	response.finish
   end
   
@@ -36,13 +41,7 @@ class Top100Albums
   	response.write(File.open("list.html", "rb").read)
   	response.write("<h2>Sorted by #{request['order'].capitalize}</h2>")
   	@info.sort_by { |x| x[request['order'].to_sym] }.each do |album|
-  		columns = "<td>#{album[:rank]}</td><td>#{album[:name]}</td><td>#{album[:year]}</td></tr>"
-  		if album[:rank] == request['rank'].to_i
-  			response_write = "<tr class=\"highlight\">" + columns
-  		else
-  			response_write = "<tr>" + columns
-  		end
-  		response.write(response_write)
+  		response.write(ERB.new(File.read("album_output.erb")).result(binding))
   	end
   	response.write(File.open("list2.html", "rb").read)
   	response.finish
