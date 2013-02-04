77c98ed3d9889c7cd3ab01c7eaf3a086b660bd1f
Travis Bybee <travis.bybee@gmail.com>
2013-02-03 22:04:59 -0700

    Changed albums_output.erb to use database instead of the array of hashes. Modified flow of outputing albums.

--- project03/albums.rb
+++ project03/albums.rb
@@ -4,14 +4,7 @@ require 'sqlite3'
 class Top100Albums
 	
 	def initialize
-		@info = Array.new
-	  	i = 1
-	  	File.open("top_100_albums.txt").read.each_line do |album|
-	  		text = album.split(",")
-	  		@info.push({ :rank=> i, :name=> text[0], :year=> text[1].strip })
-	  		i += 1
-	  	end
-      #@albums = SQLite3::Database.new("albums.sqlite3.db")
+      @albums = SQLite3::Database.new("albums.sqlite3.db")
 	end
 
   def call(env)
@@ -38,7 +31,7 @@ class Top100Albums
   
   def render_list(request)
   	response = Rack::Response.new
-  	response.write(ERB.new(File.read("album_output.erb")).result(binding))
+    response.write(ERB.new(File.read("album_output.erb")).result(binding))
   	response.finish
   end
   
