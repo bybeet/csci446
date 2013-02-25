d991e8f8fc8c57c0ec79ee0f921c5e04aa6308fc
Travis Bybee <travis.bybee@gmail.com>
2013-02-24 23:17:48 -0700

    Adding boilerplate for project04. Whoop

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c01c0421559b4b377a6f213a88f1ac8febc99e24bd106d95405a9b5e291b7742acd557c5beb4622fa59f36583f82ca18f66fc7a3c56bfd6e2d9fb97cccd93489'
