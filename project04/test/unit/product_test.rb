7748c984d61ae3f0aa13285538e8442ede5b060b
Travis Bybee <travis.bybee@gmail.com>
2013-03-05 21:16:41 -0700

    Added unit test to ensure that product titles are unique.

--- project04/test/unit/product_test.rb
+++ project04/test/unit/product_test.rb
@@ -47,4 +47,14 @@ class ProductTest < ActiveSupport::TestCase
   		assert new_product(name).invalid?, "#{name} shouldn't be valid"
   	end
   end
+
+  test "product is not valid without unique title" do
+  	product = Product.new(title: products(:ruby).title,
+			  			  description: 'yyy',
+			  			  price: 1,
+			  			  image_url: 'fred.png')
+  	assert product.invalid?
+  	assert_equal ["has already been taken"], product.errors[:title]
+  end
+
 end
