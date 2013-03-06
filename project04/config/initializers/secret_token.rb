19a1cc881e98f7eb654ed96451cbf4cd2799f3b7
Travis Bybee <travis.bybee@gmail.com>
2013-03-05 18:18:06 -0700

    Added project04 boilerplate code. Rails server working correctly.

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '2b2d2a9f8dfa649406c7df345b44a93dca3475c49e9b01cc472f024f12bd9f06480c6c7fdc8f139c90527ec004f2e47d4b7c70ea7d28bf05767ab27b2c6e4f26'
