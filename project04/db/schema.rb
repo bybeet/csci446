4dcc09d28b8f0b057e5eaa02bbe863e1ced9bd0c
Travis Bybee <travis.bybee@gmail.com>
2013-03-06 19:16:09 -0700

    Debugged error with showing cart. Was an issue with a previously stored cart session that held invalid data. Clearing session data fixed error.

--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130307015945) do
+ActiveRecord::Schema.define(:version => 20130307020926) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
